cc7f827ff736537fa2c6a857d9692ae9bd14f6c5
L-MC1 <jj.news21059@gmail.com>
2022-06-11 17:39:48 -0300

    primeiro app em vue.js

---
+++ Aula1/app.js
@@ -0,0 +1,32 @@
+// Pure js
+
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
+
+// function addGoal() {
+//   const enteredValue = inputEl.value;
+//   const listItemEl = document.createElement('li');
+//   listItemEl.textContent = enteredValue;
+//   listEl.appendChild(listItemEl);
+//   inputEl.value = '';
+// }
+
+// buttonEl.addEventListener('click', addGoal);
+
+// Vue.js
+
+Vue.createApp({
+  data: function(){
+    return {
+      objetivos: [],
+      OValor: ''
+    };
+  },
+  methods: {
+    addObjetivo(){
+      this.objetivos.push(this.OValor);
+      this.OValor = '';
+    }
+  }
+}).mount("#app");
