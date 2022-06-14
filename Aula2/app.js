12fe708b699dd3248f19cf220cbfd25ad39ebf45
L-MC1 <jj.news21059@gmail.com>
2022-06-14 12:36:56 -0300

    exercicio e novas propriedades do vue

--- Aula2/app.js
+++ Aula2/app.js
@@ -1,7 +1,21 @@
 Vue.createApp({
     data() {
       return {
-        goals: " Aprender a utilizar vue.js "
+        /// se torna global
+        goals: " Aprender a utilizar vue.js ",
+        endereco: "http://vuejs.org/",
+        /// innerHtml
+        html: "<h2> Texto de exemplo <h2>"
         };
+    },
+    methods: {
+      numrand(){
+        const random = Math.random();
+        if (random < 0.5){
+          return this.goals;
+        } else {
+          return 'Master vue!';
+        }
+      }
     }
   }).mount("#user-goal");
