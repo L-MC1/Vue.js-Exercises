932a07c9e00768b73cb76ebd3c21883106f19d14
L-MC1 <jj.news21059@gmail.com>
2022-06-14 13:11:36 -0300

    novos metodos aula 3

--- Aula3/app.js
+++ Aula3/app.js
@@ -2,8 +2,20 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: ""
     };
   },
+  methods: {
+    Sname(evento){
+      this.name = evento.target.value;
+    },
+    add(num){
+      this.counter = this.counter + num;
+    },
+    reduce(num){
+      this.counter = this.counter - num;
+    }
+  }
 });
 
 app.mount('#events');
