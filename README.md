6f0b2616fc2bf77d003088e7b7f4ff48ec8952c5
l-mc1 <lucasm.matriz@gmail.com>
2022-07-18 00:04:29 -0300

    teste2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# app1
+# teste
 
 ## Project setup
 ```
--- src/main.js
+++ src/main.js
@@ -1,15 +1,4 @@
-import { createApp } from 'vue';
+import { createApp } from 'vue'
+import App from './App.vue'
 
-/// namespace endereco das aplicacoes
-import App from './App.vue';
-import FriendContact from './components/FriendContact.vue';
-import NewFriend from './components/NewFriend.vue';
-
-const app = createApp(App);
-
-/// chamada dos componentes
-app.component('friend-contact', FriendContact);
-app.component('new-friend', NewFriend);
-
-/// chamada do app
-app.mount('#app');
+createApp(App).mount('#app')
