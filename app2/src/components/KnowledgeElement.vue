14b74cdec6ba4ed7a917b7209f96ab4296cf905f
l-mc1 <lucasm.matriz@gmail.com>
2022-07-18 22:45:52 -0300

    provide e inject

---
+++ app2/src/components/KnowledgeElement.vue
@@ -0,0 +1,15 @@
+<template>
+  <li>
+    <h3>{{ topicName }}</h3>
+    <p>{{ description }}</p>
+    <button @click="selectTopico(id)">Learn More</button>
+  </li>
+</template>
+
+<script>
+export default {
+  inject: ['selectTopico'],
+  props: ['id', 'topicName', 'description'],
+  emits: ['select-topic'],
+};
+</script>
